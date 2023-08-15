e15a65e5b88ba958e33edf35471d43aa886c9ea7
Sean Park-Ross <sean@hasura.io>
2023-08-15 19:43:22 +0200

    Updates sample app

--- migrations/default/1669033533483_init/up.sql
+++ migrations/default/1669033533483_init/up.sql
@@ -1,7 +1,4 @@
-SET standard_conforming_strings = off;
 SET check_function_bodies = false;
-SET escape_string_warning = off;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
@@ -17,21 +14,49 @@ CREATE TABLE public.cart_items (
     id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
     product_id uuid NOT NULL,
     cart_id uuid NOT NULL,
-    quantity integer NOT NULL
+    quantity integer NOT NULL,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now()
 );
 CREATE TABLE public.carts (
     id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
     user_id uuid NOT NULL,
     is_complete boolean DEFAULT false NOT NULL,
-    created_at timestamp with time zone DEFAULT now()
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now()
 );
 CREATE TABLE public.categories (
     id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
     name text NOT NULL
 );
+CREATE TABLE public.coupons (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    user_id uuid NOT NULL,
+    code text NOT NULL,
+    expiration_date timestamp with time zone NOT NULL
+);
 CREATE TABLE public.manufacturers (
     id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
     name text NOT NULL
+);
+CREATE TABLE public.notifications (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    user_id uuid NOT NULL,
+    message text NOT NULL
+);
+CREATE TABLE public.orders (
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    product_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    status text NOT NULL,
+    delivery_date timestamp with time zone,
+    is_reviewed boolean DEFAULT false NOT NULL
 );
 CREATE TABLE public.products (
     id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
@@ -41,7 +66,9 @@ CREATE TABLE public.products (
     manufacturer uuid NOT NULL,
     category uuid NOT NULL,
     image text NOT NULL,
-    country_of_origin text NOT NULL
+    country_of_origin text NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 CREATE TABLE public.reviews (
     id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
@@ -49,26 +76,18 @@ CREATE TABLE public.reviews (
     user_id uuid NOT NULL,
     rating integer NOT NULL,
     text text NOT NULL,
-    created_at timestamp with time zone DEFAULT now(),
-    updated_at timestamp with time zone DEFAULT now()
-);
-CREATE TABLE public.threads (
-    id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
-    user_id uuid NOT NULL,
-    created_at timestamp with time zone DEFAULT now()
-);
-CREATE TABLE public.messages (
-    id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
-    thread_id uuid NOT NULL,
-    user_id uuid NOT NULL,
-    body text NOT NULL,
-    created_at timestamp with time zone DEFAULT now(),
-    is_read boolean DEFAULT false NOT NULL
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 CREATE TABLE public.users (
     id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
     name text NOT NULL,
-    email text NOT NULL
+    email text NOT NULL,
+    last_seen timestamp with time zone,
+    password text,
+    is_email_verified boolean DEFAULT false,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 ALTER TABLE ONLY public.cart_items
     ADD CONSTRAINT cart_items_pkey PRIMARY KEY (id);
@@ -76,26 +95,50 @@ ALTER TABLE ONLY public.carts
     ADD CONSTRAINT carts_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.categories
     ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.coupons
+    ADD CONSTRAINT coupons_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.manufacturers
     ADD CONSTRAINT manufacturers_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.notifications
+    ADD CONSTRAINT notifications_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.products
     ADD CONSTRAINT products_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.reviews
     ADD CONSTRAINT reviews_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY public.messages
-    ADD CONSTRAINT messages_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY public.threads
-    ADD CONSTRAINT threads_pkey PRIMARY KEY (id);
+CREATE TRIGGER set_public_cart_items_updated_at BEFORE UPDATE ON public.cart_items FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_cart_items_updated_at ON public.cart_items IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_carts_updated_at BEFORE UPDATE ON public.carts FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_carts_updated_at ON public.carts IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_coupons_updated_at BEFORE UPDATE ON public.coupons FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_coupons_updated_at ON public.coupons IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_notifications_updated_at BEFORE UPDATE ON public.notifications FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_notifications_updated_at ON public.notifications IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_orders_updated_at BEFORE UPDATE ON public.orders FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_orders_updated_at ON public.orders IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_products_updated_at BEFORE UPDATE ON public.products FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_products_updated_at ON public.products IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 CREATE TRIGGER set_public_reviews_updated_at BEFORE UPDATE ON public.reviews FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_reviews_updated_at ON public.reviews IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_users_updated_at ON public.users IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 ALTER TABLE ONLY public.cart_items
     ADD CONSTRAINT cart_items_cart_id_foreign FOREIGN KEY (cart_id) REFERENCES public.carts(id);
 ALTER TABLE ONLY public.cart_items
     ADD CONSTRAINT cart_items_product_id_foreign FOREIGN KEY (product_id) REFERENCES public.products(id);
 ALTER TABLE ONLY public.carts
     ADD CONSTRAINT carts_user_id_foreign FOREIGN KEY (user_id) REFERENCES public.users(id);
+ALTER TABLE ONLY public.coupons
+    ADD CONSTRAINT coupons_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY public.notifications
+    ADD CONSTRAINT notifications_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_customer_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE;
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.products(id) ON UPDATE CASCADE;
 ALTER TABLE ONLY public.products
     ADD CONSTRAINT products_category_foreign FOREIGN KEY (category) REFERENCES public.categories(id);
 ALTER TABLE ONLY public.products
@@ -104,7 +147,3 @@ ALTER TABLE ONLY public.reviews
     ADD CONSTRAINT reviews_product_id_foreign FOREIGN KEY (product_id) REFERENCES public.products(id);
 ALTER TABLE ONLY public.reviews
     ADD CONSTRAINT reviews_user_id_foreign FOREIGN KEY (user_id) REFERENCES public.users(id);
-ALTER TABLE ONLY public.messages
-    ADD CONSTRAINT messages_thread_id_foreign FOREIGN KEY (thread_id) REFERENCES public.threads(id);
-ALTER TABLE ONLY public.messages
-    ADD CONSTRAINT messages_user_id_foreign FOREIGN KEY (user_id) REFERENCES public.users(id);
